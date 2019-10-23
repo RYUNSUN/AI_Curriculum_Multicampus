e7bfd36e7a1b9bb8acccd92aee57272f6d0518c9
RYUNSUN <lyunsun1234@naver.com>
2019-10-23 17:27:58 +0900

    191023 | telegram chatbow w/ dialogflow

---
+++ telegram_chatbot/set_webhook.py
@@ -0,0 +1,11 @@
+# set webhook.py
+# 웹훅은 한번만 걸어주면 되기 때문에, 별도의 파일로 분리해서 관리할게요.
+from decouple import config
+import requests
+
+token = config("TOKEN")
+api_url = f"https://api.telegram.org/bot{token}"
+set_webhook_url = f"{api_url}/setWebhook?url=https://lyunsun.pythonanywhere.com/992973386:AAFuOCbM3WR-iIrlhqAm4y9AivbWz_e3p-I"
+
+response = requests.get(set_webhook_url)
+print(response.text)
